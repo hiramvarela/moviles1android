b716e14173930bf129baa6965330a814a3f22001
ITIT <ITIT@ULSAs-iMac.local>
2023-08-28 18:29:46 -0600

    se agrega estructura inicial del proyecto

--- PDM123/app/src/main/java/com/example/pdm123/navigation/BarItem.kt
+++ PDM123/app/src/main/java/com/example/pdm123/navigation/BarItem.kt
@@ -1,11 +1,11 @@
-package com.example.pdm123
+package com.example.pdm123.navigation
 
 import androidx.compose.ui.graphics.vector.ImageVector
 
 //Esta es una clase de datos para los elementos del menu que aparece abajo
 data class BarItem(
     // Titulo de la opcion
-    val title : String,
+    val title : Int,
     // Icono de la opcion del menu
     val image : ImageVector,
     // Ruta a la cual se va a dirigir la opcion
